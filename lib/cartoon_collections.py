8c433a903da037fd7d1b77da47c12941b454d6f4
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-01 16:57:49 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f'{i}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.title() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteer_calls):
+    for call in planeteer_calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(words):
+    cheeses = {"cheddar": True, "gouda": True, "camembert": True}
+    for word in words:
+        if cheeses.get(word):
+            return word
